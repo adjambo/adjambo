65f49f720bcf8da120c17f156315a1dc80a2b935
N'TAPI Adjambo <12415222@crit-xubuntu>
2024-10-11 13:08:21 +0200

    Ajouter un test échoué pour la fonction affiche()

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,4 +3,7 @@ from fizzbuzz import affiche
 
 class TestFizzBuzz(unittest.TestCase):
     def test_affiche(self):
-        self.assertEqual(affiche(), "1Fizz2Buzz...")  # Mettez ici le résultat attendu
+        self.assertEqual(affiche(), "1Fizz2Buzz...")  # Remplace par le résultat attendu
+
+if __name__ == "__main__":
+    unittest.main()
